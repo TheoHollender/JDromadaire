961965bbb63c14aa011f7c359b7ad9b0da0dcdd7
MrThimote <58882120+MrThimote@users.noreply.github.com>
2021-04-13 17:37:29 +0200

    drom: number_rework: fix division 10^67/2 -> 5E-65

--- src/parser/nodes/NumberNode.java
+++ src/parser/nodes/NumberNode.java
@@ -95,7 +95,7 @@ public class NumberNode extends Node implements EvaluateOperator<NumberNode>,Una
 			return null;
 		}
 		int scaleMax = Math.max(e.getNumber().scale(), this.getNumber().scale());
-		return new NumberNode(this.getNumber().divide(e.getNumber(), scaleMax + MAX_DIVIDE_SCALE, RoundingMode.HALF_UP).stripTrailingZeros(), this.col, this.line);
+		return new NumberNode(this.getNumber().divide(e.getNumber(), scaleMax + MAX_DIVIDE_SCALE, RoundingMode.HALF_UP).stripTrailingZeros().setScale(0), this.col, this.line);
 	}
 
 	@Override
