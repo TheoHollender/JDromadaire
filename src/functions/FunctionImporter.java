ef4af1955b40ef7b9436f554bc6eeb0db8208beb
MrThimote <58882120+MrThimote@users.noreply.github.com>
2021-04-13 18:09:02 +0200

    number_rework: drom: added math library

--- src/functions/FunctionImporter.java
+++ src/functions/FunctionImporter.java
@@ -1,5 +1,9 @@
 package functions;
 
+import java.math.BigDecimal;
+import java.math.MathContext;
+
+import ch.obermuhlner.math.big.BigDecimalMath;
 import functions.base.array.MapFunction;
 import functions.base.cast.ToNumberFunction;
 import functions.base.cast.ToStringFunction;
@@ -13,6 +17,8 @@ public class FunctionImporter {
 
 	public static void ImportFunctions() {
 		VariableContext cont = EntryPoint.globalContext;
+		
+		BigDecimalMath.acos(BigDecimal.ZERO, MathContext.DECIMAL64);
 		
 		cont.setValue("print", new PrintFunction(-2, -2));
 		cont.setValue("help", new HelpFunction(-2,-2));
@@ -31,7 +37,7 @@ public class FunctionImporter {
 		cont.setValue("round", new RoundFunction(-2,-2));
 		
 		LibLoader.registerModule("openns", "libs/lib_openns.jar");
-		/*LibLoader.registerModule("math", "libs/math.jar");*/
+		LibLoader.registerModule("math", "libs/math.jar");
 		LibLoader.registerModule("time", "libs/lib_time.jar");
 		LibLoader.registerModule("random", "libs/lib_random.jar");
 	}
